45ef3be2f1d0a0988806e5d32aa2c00af78b9dee
David Golden <dagolden@cpan.org>
2014-01-08 14:14:44 -0500

    fix up distribution

--- lib/IO/Socket/INET.pm
+++ lib/IO/Socket/INET.pm
@@ -1,15 +1,21 @@
+use strict;
+use warnings;
+
+#<<< No perltidy
 package
     IO::Socket::INET; # hide from PAUSE because this module is EVIL
+#>>>
+
+our $VERSION = 1.34;
 
 use IO::Socket::IP -register;
 use base qw( IO::Socket::IP );
 use Socket qw( PF_INET );
 
-sub new
-{
-  my $class = shift;
-  return $class->SUPER::new(Family => PF_INET, PeerAddr => shift) if @_ == 1;
-  return $class->SUPER::new(Family => PF_INET, @_);
+sub new {
+    my $class = shift;
+    return $class->SUPER::new( Family => PF_INET, PeerAddr => shift ) if @_ == 1;
+    return $class->SUPER::new( Family => PF_INET, @_ );
 }
 
 1;
